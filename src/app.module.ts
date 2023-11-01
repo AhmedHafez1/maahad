8aaf5cee8230064592d115cb4806b75be63aa018
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-11-01 19:57:42 +0200

    config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,19 @@ import { AppService } from './app.service';
 import { ClassroomModule } from './class/class.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Classroom } from './class/entities/class.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ClassroomModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'example',
-      database: 'maahad',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Classroom],
       synchronize: true,
     }),
